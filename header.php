5a04fa490a76f305cb3d3e9cb3c1dc51b0d74b01
DrewTAJ <drew.t.a.johnson@gmail.com>
2016-07-26 00:27:41 -0400

    initial commit

---
+++ header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+</head>
+<body>
+    <nav>
+        <ul>
+            <li>Home</li>
+            <li>Archive</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
